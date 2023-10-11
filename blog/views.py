9b5a3bef628053579fdf911e31dee061c017f912
Davy <davyfelipe111@gmail.com>
2023-10-11 10:31:46 -0300

    ;

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render,  get_object_or_404
+from django.views.generic import DetailView, ListView, TemplateView
+import json
+from django.core.serializers.json import DjangoJSONEncoder
 
 # Create your views here.
 
 from django.http import HttpResponse
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+from django.views.decorators.csrf import csrf_exempt
 from blog.models import Post # Acrescentar
+from blog.forms import PostModelForm
+from django.contrib import messages
 
 def index(request):
     # return HttpResponse('Olá Django - index')
@@ -14,3 +22,93 @@ def ola(request): # Modificar
     posts = Post.objects.all() # recupera todos os posts do banco de dados
     context = {'posts_list': posts } # cria um dicionário com os dado
     return render(request, 'posts.html', context) # renderiza o template e passa o contexto
+
+def post_show(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    return render(request, 'post/detail.html', {'post': post})
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'post/detail.html'
+    context_object_name = 'post'
+
+def get_all_posts(request):
+    posts = list(Post.objects.values('pk', 'body_text', 'pub_date'))
+    data = {'success': True, 'posts': posts}
+    json_data = json.dumps(data, indent=1, cls=DjangoJSONEncoder)
+    response = HttpResponse(json_data, content_type='application/json')
+    response['Access-Control-Allow-Origin'] = '*' # requisição de qualquer origem
+    return response
+
+def get_post(request, post_id):
+    post = Post.objects.filter(
+        pk=post_id
+    ).values(
+        'pk', 'body_text', 'pub_date'
+    ).first()
+
+    data = {'success': True, 'post': post}
+    status = 200
+    if post is None:
+        data = {'success': False, 'error': 'Post ID não existe.'}
+        status=404
+
+    response = HttpResponse(
+        json.dumps(data, indent=1, cls=DjangoJSONEncoder),
+        content_type="application/json",
+        status=status
+    )
+
+    response['Access-Control-Allow-Origin'] = '*' # requisição de qualquer origem
+    return response
+
+class PostCreateView(CreateView):
+    model = Post
+    template_name = 'post/post_form.html'
+    # fields = ('body_text', )
+    # success_url = reverse_lazy('posts_list')
+    success_url = reverse_lazy('posts_all') # modifiquei para ir direto no template da aula do dia 20/09
+    form_class = PostModelForm
+    success_message = 'Postagem salva com sucesso.'
+    
+    # implementa o método que conclui a ação com sucesso
+    def form_valid(self, request, *args, **kwargs):
+        messages.success(self.request, self.success_message)
+        return super(PostCreateView, self).form_valid(request, *args, **kwargs)
+
+@csrf_exempt
+def create_post(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        body_text = data.get('body_text')
+        if body_text is None:
+            data = {'success': False, 'error': 'Texto do post inválido.'}
+            status = 400 # Bad Request => erro do client
+        else:
+            post = Post(body_text=body_text)
+            post.save()
+            post_data = Post.objects.filter(
+                pk=post.id
+            ).values(
+                'pk', 'body_text', 'pub_date'
+            ).first()
+        data = {'success': True, 'post': post_data}
+        status = 201 # Created
+
+    response = HttpResponse(
+        json.dumps(data, indent=1, cls=DjangoJSONEncoder),
+        content_type="application/json",
+        status=status
+    )
+
+    response['Access-Control-Allow-Origin'] = '*'
+    
+    return response
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'post/post_list.html'
+    context_object_name = 'posts'
+
+class SobreTemplateView(TemplateView):
+    template_name = 'post/sobre.html'
